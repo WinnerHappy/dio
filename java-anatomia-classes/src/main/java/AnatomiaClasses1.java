77523a27d8a2bd0406e12149b6e8dffb2b2e4504
Winner Happy <winnerhappydzn@gmail.com>
2024-05-03 11:23:00 -0300

    Aula de anatomia de classes 2

--- java-anatomia-classes/src/main/java/AnatomiaClasses1.java
+++ java-anatomia-classes/src/main/java/AnatomiaClasses1.java
@@ -1,4 +1,4 @@
-public class MinhaClasse {
+public class AnatomiaClasses1 {
     public static void main(String[] args) {
         System.out.println("Olá turma, sejam bem-vindos");
     }
---
+++ java-anatomia-classes/src/main/java/AnatomiaClasses2.java
@@ -0,0 +1,17 @@
+public class AnatomiaClasses2 {
+    public static void main(String[] args) {
+
+        // VARIAVEIS DE CONVENÇÃO UPERCASE
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+        int ANO_2024 = 2024;
+
+        // VARIAVEIS DE CONVENÇÃO CAMELCASE
+        String paisBr = "Brasil";
+        double valorPi = 3.14;
+        int estadosBrasileiros = 27;
+        int anoAtual = 2024;
+
+    }
+}
